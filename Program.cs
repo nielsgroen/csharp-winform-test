39f8cd37d9050e80a7a7659aa2ea5028a3cc55a7
Niels Groeneveld <j.c.groeneveld@students.uu.nl>
2018-09-19 15:00:18 +0200

    Created Form

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace csharp_winform_test
+{
+    class Program
+    {
+        public static void Main()
+        {
+            TestForm test = new TestForm();
+            test.Text = "Test Form";
+
+            TextBox testbox = new TextBox();
+            testbox.Location = new Point(25, 25);
+            testbox.Text = "Hanjk";
+
+            test.Controls.Add(testbox);
+
+
+
+
+            Application.Run(test);
+        }
+    }
+}
